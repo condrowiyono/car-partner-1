19eed2675dbcaf32cc81072bfaa4d09c69d56f73
Condro Wiyono <condro@outlook.co.id>
2018-11-12 09:37:53 +0700

    simple car availability query

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var carsRouter = require('./routes/cars');
+
+
+var app = express();
+
+var mysql = require("mysql");
+//Database connection
+app.use(function(req, res, next){
+	res.locals.connection = mysql.createConnection({
+		host     : 'localhost',
+		port     : 3308,
+		user     : 'root',
+		password : 'root',
+		database : 'car-partner-1'
+	});
+	res.locals.connection.connect();
+	next();
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use('/api/v1/cars', carsRouter);
+
+module.exports = app;
